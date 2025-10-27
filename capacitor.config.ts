df27e57edc5808222cadc448951e4ebb33fbf15a
Joy <134835992+Joy-Projects@users.noreply.github.com>
2025-10-27 17:19:58 +0530

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.example.wishcraft',
+  appName: 'WishCraft',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: { androidScheme: 'https' }
+}
+
+export default config
